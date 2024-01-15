scantist-ossops-m2/evince

commit 6cb4a630d175ab813895629d7ede02bea3e40d8f

shell: Don't copy whole files from remote shares
We don't need to copy whole files from non-local files unless we can't
seek around the file easily, such as for files on HTTP(S) servers.

Consider that files that can be accessed through FUSE can be seeked
around and don't need caching.

This makes loading large PDFs from a local Samba share instantaneous.

shell/ev-window.c
@@ -2416,6 +2416,7 @@ ev_window_open_uri (EvWindow       *ev_window,
 {
 	EvWindowPrivate *priv = GET_PRIVATE (ev_window);
 	GFile *source_file;
+	g_autofree char *path = NULL;
 
 	priv->in_reload = FALSE;
 
@@ -2440,13 +2441,19 @@ ev_window_open_uri (EvWindow       *ev_window,
 
 	priv->window_mode = mode;
 
+	source_file = g_file_new_for_uri (uri);
+
 	g_clear_pointer (&priv->uri, g_free);
-	priv->uri = g_strdup (uri);
+	path = g_file_get_path (source_file);
+	/* Try to use FUSE-backed files if possible to avoid downloading */
+	if (path)
+		priv->uri = g_filename_to_uri (path, NULL, NULL);
+	else
+		priv->uri = g_strdup (uri);
 
 	g_clear_object (&priv->metadata);
 	g_clear_object (&priv->bookmarks);
 
-	source_file = g_file_new_for_uri (uri);
 	if (ev_is_metadata_supported_for_file (source_file)) {
 		priv->metadata = ev_metadata_new (source_file);
 		ev_window_init_metadata_with_default_values (ev_window);
@@ -2473,13 +2480,13 @@ ev_window_open_uri (EvWindow       *ev_window,
 	setup_size_from_metadata (ev_window);
 	setup_model_from_metadata (ev_window);
 
-	priv->load_job = ev_job_load_new (uri);
+	priv->load_job = ev_job_load_new (priv->uri);
 	g_signal_connect (priv->load_job,
 			  "finished",
 			  G_CALLBACK (ev_window_load_job_cb),
 			  ev_window);
 
-	if (!g_file_is_native (source_file) && !priv->local_uri) {
+	if (path == NULL && !priv->local_uri) {
 		ev_window_load_file_remote (ev_window, source_file);
 	} else {
 		ev_window_show_loading_message (ev_window);

commit 82d12d1dde148772ce78b934ee8408093fa567c6

ev-window: Don't attempt to load non-seekable files directly
In a regression from commit 6cb4a630d1 ("shell: Don't copy whole files
from remote shares"), on some backends (including ftp and archive),
which don't allow for seeking, evince unsuccessfully attempts to open
documents directly, instead of downloading first.  Keep the existing
logic to presumably still optimize cases where seeking is allowed,
but make sure to first download files where seeking is not possible.

Fixes: https://gitlab.gnome.org/GNOME/evince/-/issues/1971

shell/ev-window.c
@@ -2381,6 +2381,27 @@ set_filenames (EvWindow *ev_window, GFile *f)
 		priv->edit_name = g_file_get_basename (f);
 }
 
+static void
+open_uri_check_local_cb (GObject      *object,
+                         GAsyncResult *res,
+                         gpointer      user_data)
+{
+	EvWindow *ev_window = user_data;
+	EvWindowPrivate *priv = GET_PRIVATE (ev_window);
+	GFile *source_file = G_FILE (object);
+	g_autoptr (GFileInputStream) source_stream = NULL;
+
+	source_stream = g_file_read_finish (source_file, res, NULL);
+
+	if (source_stream && !g_seekable_can_seek (G_SEEKABLE (source_stream))) {
+		ev_window_load_file_remote (ev_window, source_file);
+	} else {
+		ev_window_show_loading_message (ev_window);
+		g_object_unref (source_file);
+		ev_job_scheduler_push_job (priv->load_job, EV_JOB_PRIORITY_NONE);
+	}
+}
+
 void
 ev_window_open_uri (EvWindow       *ev_window,
 		    const char     *uri,
@@ -2454,9 +2475,12 @@ ev_window_open_uri (EvWindow       *ev_window,
 	if (path == NULL && !priv->local_uri) {
 		ev_window_load_file_remote (ev_window, source_file);
 	} else {
-		ev_window_show_loading_message (ev_window);
-		g_object_unref (source_file);
-		ev_job_scheduler_push_job (priv->load_job, EV_JOB_PRIORITY_NONE);
+		/* source_file is probably local, but make sure it's seekable
+		 * before loading it directly.
+		 */
+		g_file_read_async (source_file,
+				   G_PRIORITY_DEFAULT, NULL,
+				   open_uri_check_local_cb, ev_window);
 	}
 }
 